a492dd96b20c1d8a51654b14f2e2ef4fcc73975b
abhinavreddyyy <abhinavreddyyy@gmail.com>
2026-06-03 23:10:22 +0530

    Learnt exception handling

---
+++ day_13/exception_handling.py
@@ -0,0 +1,41 @@
+for index, item in enumerate([20, 30,40]):
+    print(index, item)
+
+countries = ['India', 'USA', 'UK', 'Germany']
+for word, i in enumerate(countries):
+    if i == 'UK':
+        print(f"{i} is found at index {word}")
+        break
+
+fruits = ['banana', 'orange', 'mango', 'lemon', 'lime']                    
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']
+fruits_and_veges = []
+for f, v in zip(fruits, vegetables):
+    fruits_and_veges.append({'fruit':f, 'veg':v})
+
+print(fruits_and_veges)
+
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occur')
+except ValueError:
+    print('Value error occur')
+except ZeroDivisionError:
+    print('zero division error occur')
+else:
+    print('I usually run with the try block')
+finally:
+    print('I alway run.')
+
+
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print(f'You are {name}. And your age is {age}.')
+except Exception as e:
+    print(e)
