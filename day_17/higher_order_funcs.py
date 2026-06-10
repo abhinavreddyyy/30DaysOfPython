0059677f080da69fdc926b8477b1bbb5c0aa7e6e
abhinavreddyyy <abhinavreddyyy@gmail.com>
2026-06-10 20:36:11 +0530

    Solved 8 problems on higher order functions

--- day_17/higher_order_funcs.py
+++ day_17/higher_order_funcs.py
@@ -63,9 +63,50 @@ def print_full_name(first_name, last_name, country):
     print(f"I am {first_name} {last_name}. I love to teach")
 
 print_full_name("Asabeneh", "Yetayeh", "Finland")
-
+# -------------------------
 numbers = [1,2,3,4,5]
 def square(x):
     return x * x
 squared_numbers = map(square, numbers)
 print(list(squared_numbers))
+# ---------------------
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def countries_upper(country):
+    return country.upper()
+
+upper_countries = map(countries_upper, countries)
+print(list(upper_countries))
+# ----------------
+def filter_land(countries):
+    if 'land' in countries:
+        return True
+    else:
+        return False
+    
+land_countries = filter(filter_land, countries)
+print(list(land_countries))
+# -------------
+result = list(filter(lambda country: len(country) >= 6, countries))
+print(result)
+#---------
+result_2 = list(filter(lambda country: country.startswith("E"), countries))
+print(result_2)
+#------------
+# Declare a function called get_string_lists which takes a list as a parameter and then returns a list containing only string items.
+def get_string_lists(lst):
+    return list(filter(lambda thing: isinstance(thing, str), lst))
+#---------
+from functools import reduce
+def sum_of_numbers(x, y):
+    return x + y 
+
+result_3 = reduce(sum_of_numbers, numbers)
+print(result_3)
+#-------------
+result_4 = (reduce(lambda a, b: a + ", " + b, countries[:-1]) + ", and "
+            + countries[-1] + " are north European countries")
+
+print(result_4)
